d9dd7626a8d875ff92d313a2e941390459ce506d
nique-e <niteshmule023@gmail.com>
2025-10-25 00:14:45 +0530

    Constructor overloadling two constructor sol problem

--- Constructor1.java
+++ Constructor1.java
@@ -31,7 +31,7 @@ public class Constructor1{
     public static void main(String[] args) {
         student s1 = new student();
         
-        student s2 = new student();
+        student s2 = new student("rushi", 24,21);
         s1.display();
         s2.display();
         
