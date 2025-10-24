afdcb8655567c7ce244d32ce14df7368b6287581
nique-e <niteshmule023@gmail.com>
2025-10-25 00:07:12 +0530

    display function call

--- Constructor1.java
+++ Constructor1.java
@@ -7,7 +7,17 @@ class student{
         name = "Nitesh";
         rollno = 23;
         age = 21;
-        
+
+    }
+
+    student(String n, int r, int a){
+        name = n;
+        rollno = r;
+        age = a;
+    }
+
+    void  display(){
+        System.out.println("Name :" +name);
     }
 
     
