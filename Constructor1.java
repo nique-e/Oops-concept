0ad183fc78fa6bbd97d7b107e369a32a50207733
nique-e <niteshmule023@gmail.com>
2025-10-25 00:00:22 +0530

    student are is file up data

--- Constructor1.java
+++ Constructor1.java
@@ -2,6 +2,15 @@ class student{
     String name;
     int rollno;
     int age;
+
+    student() {
+        name = "Nitesh";
+        rollno = 23;
+        age = 21;
+        
+    }
+
+    
 }
 
 public class Constructor1{
