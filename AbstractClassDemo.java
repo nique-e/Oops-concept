c146f706b7c55bcad8055a002466be37eb1cf64c
nique-e <niteshmule023@gmail.com>
2025-11-25 23:07:23 +0530

    abstract class, abstract method, and subclass implementation

---
+++ AbstractClassDemo.java
@@ -0,0 +1,38 @@
+abstract class Shape {
+
+    
+    abstract void draw();
+
+    void message() {
+        System.out.println("This is a shape.");
+    }
+}
+
+class Circle extends Shape {
+
+    
+    void draw() {
+        System.out.println("Drawing a Circle");
+    }
+}
+
+class Rectangle extends Shape {
+
+    
+    void draw() {
+        System.out.println("Drawing a Rectangle");
+    }
+}
+
+public class AbstractClassDemo {
+    public static void main(String[] args) {
+
+        Shape s1 = new Circle();     
+        Shape s2 = new Rectangle();
+
+        s1.draw();
+        s2.draw();
+
+        s1.message();  
+    }
+}
